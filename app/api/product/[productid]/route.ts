d2687921761b96d3dce52e5e8d5480eac3451240
Chigbu Chiamaka <88432604+chimka-2@users.noreply.github.com>
2025-06-11 21:06:44 +0100

    Update route.ts

--- app/api/product/[productid]/route.ts
+++ app/api/product/[productid]/route.ts
@@ -8,10 +8,11 @@ import cloudinary from '../../../utils/cloudinary';
 export async function GET(request: NextRequest, { params }: { params: Promise<{ productid: string }> }) {
     // Connect to the database
     await connectdb();
-    const resolvedParams = await params;
+  //   const resolvedParams = await params;
 
-    // Retrieve product ID from params
-   const productid = resolvedParams.productid;
+  //   // Retrieve product ID from params
+  //  const productid = resolvedParams.productid;
+   const productid = (await params).productid
 
     // Log the product ID for debugging
     console.log("Product ID:", productid);
